ef85d33e2725a7be711f0ecef7ac7e9c27cafffb
bwnyasse <risbonyasse@gmail.com>
2018-05-22 01:15:58 +0200

    Define data model according to gcl web components

---
+++ lib/md2codelab.dart
@@ -0,0 +1,4 @@
+/// convert .md files to codelab html page
+library md2codelab;
+
+export 'src/models.dart';
---
+++ lib/src/models.dart
@@ -0,0 +1,67 @@
+import 'package:html/dom.dart';
+
+/// Model related to
+/// [google codelab definition](https://github.com/googlecodelabs/codelab-components/blob/master/google-codelab.html)
+class Codelab {
+  /**
+   * Codelab title
+   */
+  final String title;
+
+  /**
+   * Steps of the codelab
+   */
+  final List<Step> steps;
+
+  /**
+   * Feedback URL for the codelab bug repors.
+   */
+  String feedbackLink;
+
+  Codelab(this.title, this.steps);
+
+  @override
+  String toString() => """
+  {
+    "title": "$title",
+    "steps": $steps,
+    "feedbackLink": $feedbackLink
+  }
+  """;
+}
+
+/// Model related to
+/// [google codelab step definition](https://github.com/googlecodelabs/codelab-components/blob/master/google-codelab-step.html).
+class Step {
+
+  /**
+   * Title of this step.
+   */
+  String label = '';
+
+  /**
+   * How long, on average, it takes to complete the step.
+   */
+  int duration = 0;
+
+  /**
+   * Indicate if it is the last of the codelab.
+   */
+  bool isLast = false;
+
+  /**
+   * The content as HTMLElement
+   */
+  Element content = new Element.html("<br/>");
+
+  Step(this.label);
+
+  @override
+  String toString() => """
+  {
+    "label": "$label",
+    "content": "${content.innerHtml}",
+    "isLast": "$isLast",
+  }
+  """;
+}
